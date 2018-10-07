d0b84841bf5e15037391b879b7e918df624e4b21
moehriegitt <github@ndwun.de>
2018-10-08 01:17:15 +0200

    add missing file

---
+++ src/test-main.c
@@ -0,0 +1,20 @@
+/* -*- Mode: C -*- */
+/* Copyright (C) 2018 by Henrik Theiling, License: GPLv3, see LICENSE file */
+
+#include <stdio.h>
+#include "test.h"
+#include "math-test.h"
+#include "dict-test.h"
+#include "list-test.h"
+#include "ring-test.h"
+
+int main(void)
+{
+    TEST_RUN(cp_math_test());
+    TEST_RUN(cp_dict_test());
+    TEST_RUN(cp_list_test());
+    TEST_RUN(cp_ring_test());
+
+    fprintf(stderr, "TEST:OK\n");
+    return 0;
+}
